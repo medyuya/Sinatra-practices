6218c32bfcd5696753f767a29c36c62f9adac2db
yuyanishimura <yuya.nishimura@medpeer.co.jp>
2023-07-11 20:26:18 +0900

    指定の投稿を削除できるようにするために、処理を追加

--- app.rb
+++ app.rb
@@ -6,6 +6,7 @@ enable :method_override
 
 get '/' do
   @memos = CSV.read("memos.csv")
+
   erb :top
 end
 
@@ -13,21 +14,40 @@ get '/memos/new' do
   erb :new
 end
 
-post '/memos/confirm' do
+post '/memos' do
   CSV.open("memos.csv", "a") do |csv|
-    csv << [CSV.read("memos.csv").count + 1, params[:title], params[:message]]
+    csv << [get_next_id, params[:title], params[:message]]
   end
   @memos = CSV.read("memos.csv")
+
   erb :top
 end
 
 get '/memos/:id' do
-  CSV.foreach("memos.csv") do |col|
-    @memo = col if col[0] == params[:id]
+  CSV.foreach("memos.csv") do |record|
+    @memo = record if record[0] == params[:id]
   end
+
   erb :show
 end
 
 delete '/memos/:id' do
+  @memos = CSV.read("memos.csv")
+  @memos.reject! { |memo| memo[0] == params[:id] }
+
+  CSV.open('memos.csv', 'w') do |csv|
+    @memos.each do |memo|
+      csv << memo
+    end
+  end
+
   erb :top
+end
+
+def get_next_id
+  memos = CSV.read("memos.csv")
+  return 1 if memos.empty?
+
+  max_id = memos.map { |memo| memo[0].to_i }.max
+  max_id + 1
 end

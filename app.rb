c4f40bfb509d21fc7d1929d6f158a7387aff306f
yuyanishimura <yuya.nishimura@medpeer.co.jp>
2023-08-18 13:52:13 +0900

    接続の処理を共通化するために変更

--- app.rb
+++ app.rb
@@ -5,13 +5,7 @@ require 'sinatra/reloader'
 require 'csv'
 require 'pg'
 
-enable :method_override
-
-get '/' do
-  redirect '/memos'
-end
-
-get '/memos' do
+def db_connection
   conn = PG.connect(
     dbname: 'postgres',
     host: 'localhost',
@@ -19,8 +13,21 @@ get '/memos' do
     sslmode: 'disable'
   )
 
-  @memos = conn.exec('SELECT * FROM users')
+  yield(conn) if block_given?
+
   conn.close
+end
+
+enable :method_override
+
+get '/' do
+  redirect '/memos'
+end
+
+get '/memos' do
+  db_connection do |conn|
+    @memos = conn.exec('SELECT * FROM users')
+  end
 
   erb :index
 end
@@ -30,74 +37,44 @@ get '/memos/new' do
 end
 
 post '/memos' do
-  conn = PG.connect(
-    dbname: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    sslmode: 'disable'
-  )
-
-  conn.exec_params('INSERT INTO users (title, memo) VALUES ($1, $2)', [params[:title], params[:message]])
-  @memos = conn.exec('SELECT * FROM users')
-  conn.close
+  db_connection do |conn|
+    conn.exec_params('INSERT INTO users (title, memo) VALUES ($1, $2)', [params[:title], params[:message]])
+    @memos = conn.exec('SELECT * FROM users')
+  end
 
   erb :index
 end
 
 get '/memos/:id' do
-  conn = PG.connect(
-    dbname: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    sslmode: 'disable'
-  )
-
-  @memo = conn.exec('SELECT * FROM users WHERE id = $1', [params[:id].to_i])
-  conn.close
+  db_connection do |conn|
+    @memo = conn.exec('SELECT * FROM users WHERE id = $1', [params[:id].to_i])
+  end
 
   erb :show
 end
 
 get '/memos/:id/edit' do
-  conn = PG.connect(
-    dbname: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    sslmode: 'disable'
-  )
-
-  @memo = conn.exec('SELECT * FROM users WHERE id = $1', [params[:id].to_i])
-  conn.close
+  db_connection do |conn|
+    @memo = conn.exec('SELECT * FROM users WHERE id = $1', [params[:id].to_i])
+  end
 
   erb :edit
 end
 
 patch '/memos' do
-  conn = PG.connect(
-    dbname: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    sslmode: 'disable'
-  )
-
-  conn.exec_params('UPDATE users SET title = $1, memo = $2 WHERE id = $3', [params[:title], params[:message], params[:id].to_i])
-  @memos = conn.exec('SELECT * FROM users')
-  conn.close
+  db_connection do |conn|
+    conn.exec_params('UPDATE users SET title = $1, memo = $2 WHERE id = $3', [params[:title], params[:message], params[:id].to_i])
+    @memos = conn.exec('SELECT * FROM users')
+  end
 
   erb :index
 end
 
 delete '/memos/:id' do
-  conn = PG.connect(
-    dbname: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    sslmode: 'disable'
-  )
-
-  conn.exec('DELETE FROM users WHERE id = $1', [params[:id].to_i])
-  @memos = conn.exec('SELECT * FROM users')
-  conn.close
+  db_connection do |conn|
+    conn.exec('DELETE FROM users WHERE id = $1', [params[:id].to_i])
+    @memos = conn.exec('SELECT * FROM users')
+  end
 
   erb :index
 end

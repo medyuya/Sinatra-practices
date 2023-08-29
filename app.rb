f33470b193ef4c0268014afb170667ee65eec112
yuyanishimura <yuya.nishimura@medpeer.co.jp>
2023-08-29 21:17:58 +0900

    DBとの接続情報をキャッシュするように変更

--- app.rb
+++ app.rb
@@ -4,29 +4,34 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'csv'
 require 'pg'
-require 'connection_pool'
-
-DB_POOL = ConnectionPool.new(size: 5, timeout: 5) do
-  PG.connect(
-    dbname: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    sslmode: 'disable'
-  )
-end
-
-before do
-  @conn = DB_POOL.with { |conn| conn }
-end
 
 enable :method_override
+
+class DatabaseConnector
+  @@conn = nil
+
+  def self.connection
+    @@conn ||= PG.connect(
+      dbname: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      sslmode: 'disable'
+    )
+  end
+end
+
+helpers do
+  def db_connection
+    DatabaseConnector.connection
+  end
+end
 
 get '/' do
   redirect '/memos'
 end
 
 get '/memos' do
-  @memos = @conn.exec('SELECT * FROM memos ORDER BY id')
+  @memos = db_connection.exec('SELECT * FROM memos ORDER BY id')
 
   erb :index
 end
@@ -37,32 +42,32 @@ end
 
 post '/memos' do
   redirect '/memos' if params[:title].strip.empty?
-  @conn.exec_params('INSERT INTO memos (title, memo) VALUES ($1, $2)', [params[:title], params[:memo]])
+  db_connection.exec_params('INSERT INTO memos (title, memo) VALUES ($1, $2)', [params[:title], params[:memo]])
 
   redirect '/memos'
 end
 
 get '/memos/:id' do
-  @memo = @conn.exec('SELECT * FROM memos WHERE id = $1', [params[:id].to_i]).first
+  @memo = db_connection.exec('SELECT * FROM memos WHERE id = $1', [params[:id].to_i]).first
 
   erb :show
 end
 
 get '/memos/:id/edit' do
-  @memo = @conn.exec('SELECT * FROM memos WHERE id = $1', [params[:id].to_i]).first
+  @memo = db_connection.exec('SELECT * FROM memos WHERE id = $1', [params[:id].to_i]).first
 
   erb :edit
 end
 
 patch '/memos/:id' do
   redirect '/memos' if params[:title].strip.empty?
-  @conn.exec_params('UPDATE memos SET title = $1, memo = $2 WHERE id = $3', [params[:title], params[:memo], params[:id].to_i])
+  db_connection.exec_params('UPDATE memos SET title = $1, memo = $2 WHERE id = $3', [params[:title], params[:memo], params[:id].to_i])
 
   redirect '/memos'
 end
 
 delete '/memos/:id' do
-  @conn.exec('DELETE FROM memos WHERE id = $1', [params[:id].to_i])
+  db_connection.exec('DELETE FROM memos WHERE id = $1', [params[:id].to_i])
 
   redirect '/memos'
 end
